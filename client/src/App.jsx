68831441dda8926bc6003721f01acd56eb9df7df
DangQuang98UIT <16521001@gm.uit.edu.vn>
2022-03-02 23:37:18 +0700

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+//import Home from "./pages/Home";
+//import Product from "./pages/Product";
+//import Login from "./pages/Login";
+//import ProductList from "./pages/ProductList";
+//import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+
+const App = () => {
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
+};
+
+export default App;
